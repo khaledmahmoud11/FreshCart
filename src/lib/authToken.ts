729714f8b6b50f3f5ff96b612e95cc2e4127f234
Khaled Mahmoud <khaledmahmouda42@gmail.com>
2026-04-21 18:12:26 +0200

    fix get tojen

--- src/lib/authToken.ts
+++ src/lib/authToken.ts
@@ -3,14 +3,14 @@ import { cookies } from "next/headers";
 
 export async function getUserToken(){
     
-    const decodedToken = (await cookies()).get("next-auth.session-token")?.value;
+    const decodedToken = (await cookies()).get("__Secure-next-auth.session-token")?.value;
     const token = await decode({token:decodedToken,secret:process.env.AUTH_SECRET !})
     return token?.token;
 
 }
 export async function getUserid(){
     
-    const decodedToken = (await cookies()).get("next-auth.session-token")?.value;
+    const decodedToken = (await cookies()).get("__Secure-next-auth.session-token")?.value;
     const token = await decode({token:decodedToken,secret:process.env.AUTH_SECRET !})
     return token?.sub;
 
