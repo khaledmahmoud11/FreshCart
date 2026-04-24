e9f7a6c796b0fc223a56769336f8489f6a985ce8
Khaled Mahmoud <khaledmahmouda42@gmail.com>
2026-04-24 16:26:39 +0200

    change general loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,19 @@
-import { Avatar } from '@/components/ui/avatar'
+import logo from "../../../assets/freshcart-logo.49f1b44d (1).png"
 import React from 'react'
 import { Spinner } from "@/components/ui/spinner"
+import Image from 'next/image'
 
 export default function Loadind() {
   return (
     <div className='h-screen flex flex-column gap-3 items-center justify-center '>
         <div className="nav-logo flex items-center gap-2 text-2xl font-bold">
-            <Avatar className='flex items-center justify-center rounded-xl bg-black text-white'>S</Avatar>
+            <Image
+                src={logo}
+                alt='logo'
+                width={1000}
+                height={1000}
+                className='w-40 mb-4 cursor-pointer'
+            />
             <div>FreshCart</div>
         </div>
         <Spinner className='size-8' />
