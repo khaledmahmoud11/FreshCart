b3b6066854c9d233e5759bd3b9e2023bc1e6e1a0
Khaled Mahmoud <khaledmahmouda42@gmail.com>
2026-05-05 19:52:56 +0200

    fixing typescript errors for swipper

--- src/app/products/[productDetails]/page.tsx
+++ src/app/products/[productDetails]/page.tsx
@@ -20,20 +20,18 @@ import {
 
 
 
-import { Check, Eye, Repeat2Icon, Share2, ShoppingBasket, Star,  Truck,  Zap } from 'lucide-react';
+import { Check,  Share2, ShoppingBasket, Star,  Truck,  Zap } from 'lucide-react';
 import ProductCounter from '@/app/Components/Commens/ProductCounter';
 import WishBtnInDetails from '@/app/Components/Commens/WishBtnInDetails';
 import ProductGallery from '@/app/Components/Commens/ProductSlider';
 import { Product, Products } from '@/types/productInterface'
 import ProductsShipings from '@/app/Components/Commens/ProductsShipings'
 import { getAllProducts } from '@/services/products'
-import AddToCartBtn from '@/app/Components/Commens/AddToCartBtn'
-import Image from 'next/image'
-import AddToWishBtn from '@/app/Components/Commens/AddToWishBtn'
 import AddCartFromDetails from '@/app/Components/Commens/AddCartFromDetails'
 import { getReviews } from '@/services/reviews'
 import { IReview, IReviewsResponse } from '@/types/reviews'
 import ProductsReviews from '@/app/Components/Commens/ProductsReviews'
+import ProductItem from '@/app/Components/Commens/ProductItem'
 export default async function ProdctDetails({
   params,
 }: {
@@ -309,64 +307,7 @@ export default async function ProdctDetails({
                 <div className="my-4  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
                     {relatedProducts.map((item)=>
                       <React.Fragment key={item._id} >
-                        <div className=" col-span-1 relative hover:shadow-md hover:-translate-y-1.5 duration-400 transition-all rounded-2xl ">
-                          {item.priceAfterDiscount && 
-                            <div className="absolute rounded-lg text-sm bg-red-500 font-bold text-white px-2 py-1 top-5 left-5">
-                              {Math.round(((item.priceAfterDiscount-item.price)/item.price)*100)} %
-                            </div>
-                          }
-                          
-                          <div className="absolute top-5 right-0  space-y-4">
-                            <AddToWishBtn productId={item._id} />
-                            <div className="text-gray-600 cursor-pointer hover:text-green-700 transition-all duration-200"><Repeat2Icon size={20} /></div>
-                            <div className="text-gray-600 cursor-pointer hover:text-green-700 transition-all duration-200"><Eye size={20} /></div>
-                          </div>
-                          <Card >
-                            <Link href={`/products/${item._id}`}>
-                              <Image  
-                                src={item.imageCover} 
-                                width={1000} 
-                                height={1000}
-                                alt='item_image' 
-                                className='w-full object-contain h-60 '
-                                loading="eager"
-                                fetchPriority="high"  
-                              />
-                              <CardHeader>
-                                <div className="brand text-gray-400 text-lg">
-                                  {item.brand.name}
-                                </div>
-                                <CardTitle title={item.title} className="text-xl font-bold">
-                                  {item.title.split(" ").length > 5
-                                    ? item.title.split(" ").slice(0, 5).join(" ") + "..."
-                                    : item.title}
-                                </CardTitle>
-                                <CardDescription>{item.category.name}</CardDescription>
-                                <div className="flex items-center gap-2">
-                                  <div className="flex items-center gap-2">
-                                    {[0,1,2,3,4].map((index)=>{
-                                      const filledStar = index < Math.floor(item.ratingsAverage)
-                                      return<React.Fragment key={index}>
-                                        <Star size={16} className= {filledStar ?"text-yellow-500 fill-yellow-500 ":"text-gray-500 fill-gray-500" }/>
-                                      </React.Fragment>
-                                    
-                                      }
-                                    )}
-                                  </div>
-                                  <p className='text-gray-400'> {item.ratingsAverage} </p>
-                                  <p className='text-gray-400'>( {item.ratingsQuantity} )</p>
-                                </div>
-                              </CardHeader>
-                            </Link>
-                            <CardContent className="flex items-center justify-between">
-                              {item.priceAfterDiscount && <p className='text-xl font-bold text-green-600' >{item.priceAfterDiscount} Egp</p> }
-                              <p className={` ${item.priceAfterDiscount ? "text-gray-600 text-md line-through" : "text-lg"}  font-bold`}>{item.price} Egp</p>
-                              <div >
-                                <AddToCartBtn prodId ={item._id} />
-                              </div>
-                            </CardContent>
-                          </Card>
-                        </div>
+                        <ProductItem product={item}/>
                       </React.Fragment>
                     )
                     }
