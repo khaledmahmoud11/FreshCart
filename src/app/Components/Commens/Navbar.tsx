9d5a57929e3ec481f52b17c57849f6739eab4053
Khaled Mahmoud <khaledmahmouda42@gmail.com>
2026-05-06 16:34:30 +0200

    add classes for sign in button in navbar to be appeared in lg screen only

--- src/app/Components/Commens/Navbar.tsx
+++ src/app/Components/Commens/Navbar.tsx
@@ -143,7 +143,7 @@ export default function Navbar() {
             
             {status === "unauthenticated" ?  
             <>
-              <Link href="/login" className='text-white text-md bg-green-600 hover:bg-green-800 rounded-3xl px-4 py-3 flex items-center justify-center gap-2 font-bold '> <UserRound size={16} /> Sign In</Link>          
+              <Link href="/login" className='text-white text-md bg-green-600 hover:bg-green-800 rounded-3xl px-4 py-3 items-center justify-center gap-2 font-bold hidden lg:flex '> <UserRound size={16} /> Sign In</Link>          
             </> 
             :
             <>
